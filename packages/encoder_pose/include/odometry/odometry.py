bc45eda02c036ac9e68fd066b687b115e6bdebb9
adamburhan <adam14011999@gmail.com>
2024-10-14 20:54:52 -0400

    testing odometry

--- packages/encoder_pose/include/odometry/odometry.py
+++ packages/encoder_pose/include/odometry/odometry.py
@@ -15,8 +15,9 @@ def delta_phi(ticks: int, prev_ticks: int, resolution: int) -> Tuple[float, floa
     """
 
     # TODO: these are random values, you have to implement your own solution in here
-    ticks = prev_ticks + int(np.random.uniform(0, 10))
-    dphi = np.random.random()
+    ticks = ticks - prev_ticks 
+    alpha = 2 * np.pi / resolution
+    dphi = ticks * alpha
     # ---
     return dphi, ticks
 
@@ -50,9 +51,17 @@ def estimate_pose(
         theta_curr:              estimated heading
     """
 
+    d_left = R * delta_phi_left
+    d_right = R * delta_phi_right
+    d_A = (d_left + d_right)/2
+    delta_theta = (d_right - d_left)/baseline
+    delta_x = d_A * np.cos(theta_prev)
+    delta_y = d_A * np.sin(theta_prev)
+    
+
     # These are random values, replace with your own
-    x_curr = np.random.random()
-    y_curr = np.random.random()
-    theta_curr = np.random.random()
+    x_curr = x_prev + delta_x
+    y_curr = y_prev + delta_y
+    theta_curr = theta_prev + delta_theta 
     # ---
     return x_curr, y_curr, theta_curr
